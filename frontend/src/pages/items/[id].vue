d948543f1d22a357e44de23df44924ab7e047483
Martin Zen <martin@rockin.dev>
2024-04-20 18:13:29 -0300

    Frontend basic structure

---
+++ frontend/src/pages/items/[id].vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Item Details</h1>
+</template>
+
+<script setup>
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+
+console.debug(route)
+</script>
--- frontend/src/pages/items/index.vue
+++ frontend/src/pages/items/index.vue
@@ -1,3 +1,3 @@
 <template>
-  <RouterView />
+  <h1>Results</h1>
 </template>
